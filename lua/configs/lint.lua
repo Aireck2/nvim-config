957b876846523726afbc7e61834e4ac355625fa4
@erick <erickescribaa@gmail.com>
2026-05-30 18:01:20 -0500

    feat: add lint config

---
+++ lua/configs/lint.lua
@@ -0,0 +1,14 @@
+local lint = require "lint"
+
+lint.linters_by_ft = {
+  python = { "pylint" },
+  javascript = { "eslint_d" },
+  typescript = { "eslint_d" },
+}
+
+-- Create an autocmd to lint on save
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  callback = function()
+    require("lint").try_lint()
+  end,
+})
