318360b55e6e688abcb094c22cce545b8274a9c7
@erick <erickescribaa@gmail.com>
2026-06-04 16:12:18 -0500

    feat(lspconfig): add terraformls, lua_ls, bashls, dockerls, yamlls, prettypst

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,12 +4,17 @@ local servers = {
   "html",
   "cssls",
   "jsonls",
-  "tsserver",
   "clangd", -- C/C++
   "ts_ls", -- TS, JS, JSX, TSX
   "pyright", -- Python
   "marksman", -- Markdown
   "texlab", -- LaTeX
+  "prettypst",
+  "terraformls", -- Terraform
+  "lua_ls", -- <-- Recomendado: Para configurar tu propio Neovim sin errores de "global vim"
+  "bashls", -- <-- Recomendado: Para scripts de terminal (.sh)
+  "dockerls", -- <-- Recomendado: Para Dockerfiles
+  "yamlls", -- <-- Recomendado: Para archivos YAML / Kubernetes
 }
 vim.lsp.enable(servers)
 
