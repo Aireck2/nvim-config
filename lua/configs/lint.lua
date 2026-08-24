9c03304dcd167a112a14cb5e98baaa848d2d375a
@erick <erickescribaa@gmail.com>
2026-08-24 01:02:58 -0500

    feat(lint): structure linters by language ecosystem with safe autocmd

--- lua/configs/lint.lua
+++ lua/configs/lint.lua
@@ -1,15 +1,31 @@
+-- ============================================================================
+-- NVIM-LINT (Asynchronous Linting Configuration)
+-- ============================================================================
+-- Linters run on `BufWritePost`, `BufReadPost`, and `InsertLeave`.
+-- To DISABLE linting for a specific language (e.g. Java, JS when coding Python),
+-- comment out or remove the filetype entry from `linters_by_ft` below.
+-- ============================================================================
+
 local lint = require "lint"
 
 lint.linters_by_ft = {
+  -- --- Python ---
   python = { "pylint" },
+
+  -- --- Web Development / JavaScript / TypeScript ---
   javascript = { "eslint_d" },
   typescript = { "eslint_d" },
+  javascriptreact = { "eslint_d" },
+  typescriptreact = { "eslint_d" },
+
+  -- --- Java ---
   java = { "checkstyle" },
 }
 
--- Create an autocmd to lint on save
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+-- Autocommand to trigger linting automatically
+vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+  group = vim.api.nvim_create_augroup("nvim-lint-auto", { clear = true }),
   callback = function()
-    require("lint").try_lint()
+    lint.try_lint()
   end,
 })
